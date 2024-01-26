b3da76ea5189ad408b4644d808c2dc013ab2b3d1
“Carina <ksfraan@hotmail.com>
2024-01-26 12:48:31 +0000

    login and signup pages working

--- GrooveGrid/src/components/Navbar.jsx
+++ GrooveGrid/src/components/Navbar.jsx
@@ -1,21 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import logo from '../assets/Logo3.png';
 
 function Navbar() {
-    return ( 
+    return (
         <nav>
             <Link to='/'>
-                <img src={logo} alt='Groove Grid' />  
+                <img src={logo} alt='Groove Grid' />
             </Link>
             <Link to='/genre'>Genre</Link>
             <Link to='/artists'>Artists</Link>
             <Link to='/aboutus'>About Us</Link>
             <div>
-                <Link to='/singup'>Signup</Link>
+                <Link to='/signup'>Signup</Link>
                 <Link to='/login'>Login</Link>
-            </div>                      
+            </div>
         </nav>
-     );
+    );
 }
 
 export default Navbar;
