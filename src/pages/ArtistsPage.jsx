4d9d9734b0dd434a7a8a30233efffb8c494fa3a0
chuinga <mfproencamartins@gmail.com>
2024-01-26 15:59:50 +0000

    changes

--- src/pages/ArtistsPage.jsx
+++ src/pages/ArtistsPage.jsx
@@ -1,19 +1,20 @@
 import React, { useState, useEffect } from "react";
 import ArtistList from "../components/ArtistList.jsx";
 
+
 const ArtistsPage = () => {
   const [artists, setArtists] = useState([]);
 
   // Function to fetch artists from the backend
   const fetchArtists = async () => {
     try {
+
       /* const baseUrl= import.meta.env.VITE_API_URL.endsWith("/") 
         ? import.meta.env.VITE_API_URL.slice(0,-1)
         : import.meta.env.VITE_API_URL */
 
-      const response = await fetch(
-        `${import.meta.env.VITE_API_URL}/api/artists`
-      );
+
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/api/artists`);
       if (!response.ok) throw new Error("Data fetch failed");
       const data = await response.json();
       setArtists(data);
