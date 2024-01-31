8451c53202c17c4b5310a1b23a59ce4ffb51fd21
“Carina <ksfraan@hotmail.com>
2024-01-31 12:33:47 +0000

    Modal CSS

--- src/components/CreateArtistForm.jsx
+++ src/components/CreateArtistForm.jsx
@@ -1,67 +1,67 @@
-import React, { useState } from "react";
+import { useState } from 'react';
 
 const CreateArtistForm = ({ onCreateArtist, genres }) => {
-  const [artistData, setArtistData] = useState({
-    name: "",
-    genre: "",
-    image: "",
-  });
-
-  const handleInputChange = (e) => {
-    setArtistData({
-      ...artistData,
-      [e.target.name]: e.target.value,
+    const [artistData, setArtistData] = useState({
+        name: '',
+        genre: '',
+        image: '',
     });
-  };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    onCreateArtist(artistData);
-    // Reset the form fields after successful creation
-    setArtistData({
-      name: "",
-      genre: "",
-      image: "",
-    });
-  };
+    const handleInputChange = (e) => {
+        setArtistData({
+            ...artistData,
+            [e.target.name]: e.target.value,
+        });
+    };
 
-  return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Name:
-        <input
-          type="text"
-          name="name"
-          value={artistData.name}
-          onChange={handleInputChange}
-        />
-      </label>
-      <label>
-        Genre:
-        <select
-          name="genre"
-          value={artistData.genre}
-          onChange={handleInputChange}
-        >
-          {genres.map((genre) => (
-            <option value={genre._id} key={genre._id}>
-              {genre.name}
-            </option>
-          ))}
-        </select>
-      </label>
-      <label>
-        Image:
-        <input
-          type="text"
-          name="image"
-          value={artistData.image}
-          onChange={handleInputChange}
-        />
-      </label>
-      <button type="submit">Create Artist</button>
-    </form>
-  );
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onCreateArtist(artistData);
+        // Reset the form fields after successful creation
+        setArtistData({
+            name: '',
+            genre: '',
+            image: '',
+        });
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className='create-artist-form-wrapper'>
+            <label>
+                Name:
+                <input
+                    type='text'
+                    name='name'
+                    value={artistData.name}
+                    onChange={handleInputChange}
+                />
+            </label>
+            <label>
+                Genre:
+                <select
+                    name='genre'
+                    value={artistData.genre}
+                    onChange={handleInputChange}
+                >
+                    {genres.map((genre) => (
+                        <option value={genre._id} key={genre._id}>
+                            {genre.name}
+                        </option>
+                    ))}
+                </select>
+            </label>
+            <label>
+                Image:
+                <input
+                    type='text'
+                    name='image'
+                    value={artistData.image}
+                    onChange={handleInputChange}
+                />
+            </label>
+            <button type='submit'>Create Artist</button>
+        </form>
+    );
 };
 
 export default CreateArtistForm;
