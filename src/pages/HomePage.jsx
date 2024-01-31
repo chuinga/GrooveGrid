ed06bbd678abc0bf03d703e083227d5cbd897056
chuinga <mfproencamartins@gmail.com>
2024-01-31 11:58:01 +0000

    Styling Homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,29 +1,26 @@
 import { Link } from 'react-router-dom';
-
 import '../styles/HomePage.css';
 
 const HomePage = () => {
     return (
-        <div className='home-page-wrapper'>
+        <>
+        <div className='title'>
             <h1>Welcome to GrooveGrid</h1>
-            <p>
-                Explore a wide range of music genres, albums, and songs from
-                various artists.
-            </p>
-            <p>
-                <Link to='/genre'>Browse Genres</Link>
-            </p>
-            <p>
-                <Link to='/artists'>Browse Artists</Link>
-            </p>
-            <p>
-                <Link to='/albums'>Browse Albums</Link>
-            </p>
-            <p>
-                <Link to='/songs'>Browse Songs</Link>
-            </p>
+<p>Explore a wide range of music genres, albums, 
+and songs from various artists.</p>
         </div>
+        <div className='image-wrapper'>            
+            <Link to='/genre' className='image-link'></Link>
+            <Link to='/artists' className='image-link'></Link>
+        </div>
+        </>
+        
     );
+        
+        
 };
 
 export default HomePage;
+/* 
+
+ */
